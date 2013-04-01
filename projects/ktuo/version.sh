44756fb7ef54a2806bc99887056962326a09b63b
Vladimir Kirichenko <vladimir.kirichenko@gmail.com>
2013-04-01 04:28:38 +0300

    erlandox

--- projects/ktuo/version.sh
+++ projects/ktuo/version.sh
@@ -1,2 +1,4 @@
-VSN=`grep -oe 'vsn,\s*\"\(.*\)\"' ../ktuo/ebin/ktuo.app | sed -r 's|[^\"]*\"(.\..\..)\..\"|\1|'`
-echo -n $VSN.`printf "%d" $BUILD_NUMBER`
+#VSN=`grep -oe 'vsn,\s*\"\(.*\)\"' ../ktuo/ebin/ktuo.app | sed -r 's|[^\"]*\"(.\..\..)\..\"|\1|'`
+VSN=`grep -oe 'vsn,\s*\"\(.*\)\"' ../ktuo/ebin/ktuo.app | sed -r 's|[^\"]*\"([^\"]*)\"|\1|'`
+echo -n $VSN
+
