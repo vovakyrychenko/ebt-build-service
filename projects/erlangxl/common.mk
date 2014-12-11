cdf3227adb4e50abe7011fbcec692d414f8810a7
Vladimir Kirichenko <vladimir.kirichenko@gmail.com>
2014-12-11 17:35:00 +0200

    temp old build

---
+++ projects/erlangxl/common.mk
@@ -0,0 +1,19 @@
+all: ${PACKAGE} git-update ebt publish
+
+git-update:
+#	cd ${PACKAGE} && git pull 
+	
+ebt:
+	cd ${PACKAGE} && ebt -o ../out
+
+${PACKAGE}: 
+	git clone ${REPO} ${PACKAGE}
+	[ ! -z "${BRANCH}" ] && cd ${PACKAGE} && git checkout ${BRANCH} || echo no branch
+	make prepare
+
+publish:
+	../../publish "${PACKAGE} ${REPO}" out/dist
+
+clean:
+	rm -rf ${PACKAGE}
+	rm -rf out
